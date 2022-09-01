b53db837ca08c675a120336c596277d1a5a9a8c4
Your name here <elia.orsini@hotmail.com>
2022-09-01 12:59:29 +0100

    added ratings/status

--- components/film.js
+++ components/film.js
@@ -3,9 +3,12 @@ export default function Film(props) {
     return (
       <div className="mt-10 mx-auto">
         <a href={props.link} target="_blank" rel="noreferrer">
-            <img src={props.img} className="w-88 border border-black"/>
+            <img src={props.img} className="w-88 border border-black z-0"/>
         </a>
-        <h1 className="font-black text-lg uppercase mt-2">{props.title}</h1>
+        <h1 className="font-black text-lg uppercase mt-2 tracking-tight">{props.title}
+        {props.status ? (<span className="font-extralight tracking-wide text-lg uppercase ml-1">{props.status}</span>) : null}
+        {props.rating ? (<img className="w-20 ml-0 -mt-3" src={`${props.rating}stars.png`} />) : null}
+        </h1>
       </div>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,30 @@ import { Client } from "@notionhq/client";
 import Film from '../components/film'
 
 const IndexPage = ({ items }) => {
+  console.log(items)
 
   return (
-    <div>
-        <h1 className="font-bold text-xl text-center uppercase">el&apos;s films</h1>
-        <h3 className="font-bold text-xs text-center lowercase">
-          a list of films i want to watch
-        </h3>
-        <div className="mx-auto my-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 sm:px-10 px-2">
-          {items.map((obj)=> {
-            return (
-              <Film 
-                key={obj.id}
-                title={obj.properties.Name.title[0].plain_text} 
-                link={obj.properties.URL.url} 
-                img={obj.properties.IMG.url} 
-              />
-            )  
-          })}
+        <div>
+          <img className="absolute ml-2 sm:ml-6 w-10 mt-1" src="IMG_3400.JPG" />
+          <h1 className="font-bold text-xl text-center uppercase tracking-wide">el&apos;s films</h1>
+          <h3 className="text-xs text-center uppercase font-light tracking-tighter">
+            a list of films i want to watch
+          </h3>
+          <div className="mx-auto my-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 sm:px-10 px-2 mb-20">
+            {items.map((obj)=> {
+              return (
+                <Film 
+                  key={obj.id}
+                  title={obj.properties.Name.title[0].plain_text} 
+                  link={obj.properties.URL.url} 
+                  img={obj.properties.IMG.url} 
+                  status={obj.properties.STATUS.select ? obj.properties.STATUS.select.name : null}
+                  rating={obj.properties.RATING.number}
+                />
+              )  
+            })}
         </div>
+        <p className="text-center text-xs mb-10"> made by el © 2022</p>
     </div>
   );
 };
