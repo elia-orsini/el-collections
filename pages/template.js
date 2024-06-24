7f75f69f52b3f91f89c8e2c21b46986567b32636
Your name here <elia.orsini@hotmail.com>
2024-06-24 09:17:28 +0100

    fix: template animation

--- pages/template.js
+++ pages/template.js
@@ -8,16 +8,21 @@ const Template = ({ children }) => {
 
   useGSAP(() => {
     const handleRouteComplete = () => {
-      const timeline = gsap.timeline();
+      const banners = gsap.utils.toArray("#blackBanner");
 
-      timeline.to(["#banner-1", "#banner-2", "#banner-3", "#banner-4"], {
-        yPercent: 0,
-        duration: 0,
-      });
-      timeline.to(["#banner-1", "#banner-2", "#banner-3", "#banner-4"], {
-        yPercent: 100,
-        stagger: 0.2,
-      });
+      gsap.fromTo(
+        banners,
+        {
+          yPercent: 0,
+          duration: 0,
+        },
+        {
+          yPercent: 200,
+          stagger: 0.2,
+          duration: 2,
+          delay: 0.1,
+        }
+      );
 
       return () => {
         timeline.kill();
@@ -37,19 +42,19 @@ const Template = ({ children }) => {
     <>
       <div>
         <div
-          id="banner-1"
+          id="blackBanner"
           className="min-h-screen bg-black z-20 fixed top-0 left-0 w-1/4"
         ></div>
         <div
-          id="banner-2"
+          id="blackBanner"
           className="min-h-screen bg-black z-20 fixed top-0 left-1/4 w-1/4"
         ></div>
         <div
-          id="banner-3"
+          id="blackBanner"
           className="min-h-screen bg-black z-20 fixed top-0 left-2/4 w-1/4"
         ></div>
         <div
-          id="banner-4"
+          id="blackBanner"
           className="min-h-screen bg-black z-20 fixed top-0 left-3/4 w-1/4"
         ></div>
       </div>
