b3c459c5e023c7370cc7ec337a458f40c24f9694
Your name here <elia.orsini@hotmail.com>
2024-06-27 11:25:17 +0100

    feat: move singlePage book to app router

---
+++ app/books/[slug]/page.js
@@ -0,0 +1,37 @@
+import "react-notion/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+import { NotionRenderer } from "react-notion";
+
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import Title from "@components/Title";
+
+async function getData(slug) {
+  const data = await fetch(
+    `https://notion-api.splitbee.io/v1/table/${slug}`
+  ).then((res) => res.json());
+
+  return { bookContent: data };
+}
+
+export default async function Page({ params }) {
+  const { bookContent } = await getData(params.slug);
+
+  console.log(bookContent);
+
+  return (
+    <div className="min-h-screen flex-col flex">
+      <Header title="blog post" description="..." />
+
+      <Title leftSide={<span>title</span>} />
+
+      <div className="h-full my-10 mx-auto w-11/12 sm:w-8/12 justify-star">
+        <div>
+          <NotionRenderer blockMap={bookContent} />
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
